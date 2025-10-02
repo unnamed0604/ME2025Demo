9b1bfafce174e897464b13b814354e60be016c74
unnamed0604 <tericb0604@gmail.com>
2025-10-02 13:25:32 +0800

    create project files

---
+++ HW5_Javascript2/static/113409537_js_3.js
@@ -0,0 +1,23 @@
+const answer = Math.floor(Math.random() * 101);
+console.log(answer);
+var correct = 0;
+var count = 1
+
+function guess(){
+    let input_num = document.forms['Guessnum']['Guess'].value;
+
+    console.log(input_num);
+    if(input_num > answer){
+        alert("So big");
+        count = count+1;
+    }
+    else if( input_num < answer){
+        alert("So small");
+        count = count+1;
+    }
+    else{
+        alert("Correct, U have try"+count+"times." )
+        count = 1
+        const answer = Math.floor(Math.random() * 101);
+    }
+}
