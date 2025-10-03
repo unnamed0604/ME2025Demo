152a3c709f96eb5736c1488252d4a586bb47bebd
unnamed0604 <tericb0604@gmail.com>
2025-10-03 19:38:57 +0800

    Finish the project1, fix timer bug and add time record

--- HW5_Javascript2/static/113409537_js_3.js
+++ HW5_Javascript2/static/113409537_js_3.js
@@ -5,15 +5,19 @@ var count = 0;
 var title = document.getElementById('title')
 var new_element = document.createElement('h3')
 var timer = document.createElement('h2')
+var guess_record = document.getElementById('record')
+var record = document.createElement('li')
+
+
 timer.textContent = "Time: "
 title.appendChild(timer)
 
 var time_num = 0
-
 var reset_flag =0
-
 var start;
 var hint_flag =0
+var guess_times = 0
+var now
 
 
 function reset(){
@@ -21,10 +25,13 @@ function reset(){
     answer = Math.floor(Math.random() * 101);
     console.log(answer);
 
+
     count = 0
-    clearInterval(start)
+    time_num = 0
+    console.log(time_num)
+    timer.textContent = "Time: "+time_num
     reset_flag = 1
-    start = setInterval(counting_time, 1000)
+    clearInterval(start)
     hint_flag = 0
 
 
@@ -32,8 +39,11 @@ function reset(){
 function counting_time(){
     time_num++;
     console.log(time_num)
+
     if(reset_flag ==1){
         time_num = 0;
+        console.log(time_num)
+        timer.textContent = "Time: "+time_num
         reset_flag=0
     }
     timer.textContent = "Time: "+time_num
@@ -79,8 +89,16 @@ function guess(){
     }
     else{
         count= count+1;
+        guess_times++;
+        now = new Date()
+        now = now.toLocaleTimeString()
 
-        alert("Correct, U have try "+count+" times." )
+        alert("Correct, U have try "+count+" times and spent "+time_num+"secs." )
+
+        record = document.createElement('li')
+        record.textContent = guess_times+'. U have try '+count+" times and spend "+time_num+"secs."+now
+        guess_record.appendChild(record)
+        time_num=0
 
         reset()
 
